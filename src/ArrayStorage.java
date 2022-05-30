28b6704a90da2aa403373c038eafbeaacc59918c
madlur <2185430@gmail.com>
2022-05-30 12:02:03 +0300

    Доработка по замечаниям @chimax: 1. добавлено поле size; 2. изменен метод ArrayStorage.clear; 3. переработан метод ArrayStorage.save; 4. переработан метод ArrayStorage.getAll; 5. переработан метод ArrayStorage.size;

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,17 +5,17 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        if (storage[0] == null) storage[0] = r;
-        else {
-            System.arraycopy(storage, 0, storage, 1, 9999);
-            storage[0] = r;
-        }
+        storage[size] = r;
+        size++;
+
     }
 
     String get(String uuid) {
@@ -36,26 +36,17 @@ public class ArrayStorage {
             }
         }
         System.arraycopy(storage, index + 1, storage, index, storage.length - index - 1);
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int counter = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) counter++;
-        }
-        Resume[] newStorage = new Resume[counter];
-        System.arraycopy(storage, 0, newStorage, 0, counter);
-        return newStorage;
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int counter = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) counter++;
-        }
-        return counter;
+        return size;
     }
 }
